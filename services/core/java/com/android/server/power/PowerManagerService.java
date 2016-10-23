2b933eea321857a03294bbc6e23777377373c4b6
Carlo Savignano <carlosavignano@aospa.co>
2016-10-23 18:41:27 +0800

    pocket: introduce pocket judge
    * Judge if device is in pocket.
    * Notify clients callbacks when pocked state changes.
    * Start listening when device becomes not interactive.
    * Stop listening when device becomes interactive and is NOT in pocket.
    
    From: Carlo Savignano <carlosavignano@aospa.co>
    Date: Wed, 6 Sep 2017 03:53:16 -0400
    Subject: policy: introduce pocket lock
    
    * Block touch screen and keys inputs when device is in pocket.
    * Safe-door to disable pocket lock within long press power button
    if sensors do not behave correctly after
    PocketManager.isDeviceInPocket()
    returns true.
    * Window uses TYPE_SYSTEM_ERROR flag with ensure high
    priority over the lockscreen and other high priority windows.
    * Do not illuminate buttons when pocket lock is showing.
    * Disable fingerprint events and gestures when in pocket
    * Be friendly towards Keypress Boost
    
    Contributors:
    Carlo Savignano <carlosavignano@aospa.co>
    Chris Lahaye <dev@chrislahaye.com>
    Anas Karbila <anaskarbila@aospa.co>
    Alex Naidis <alex.naidis@linux.com>
    Park Ju Hyung <qkrwngud825@gmail.com>
    
    Ticket: NOUGAT-15
    
    Updated for 8.0 by Sirhc95
    
    From: Alex Naidis <alex.naidis@linux.com>
    Date: Mon, 14 Aug 2017 19:51:27 -0400
    Subject: PhoneWindowManager: Block screenshots when pocket lock is
     showing
    
    We should prevent accidental screenshots while pocket lock
    is showing.
    
    From: Alex Naidis <alex.naidis@linux.com>
    Date: Mon, 14 Aug 2017 20:56:16 -0400
    Subject: PocketService: Adjust light sensor rate to 400ms
    
    We already adjusted the proximity sensor rate to
    400ms, so adjust the rate for the light sensor
    accordingly and create a tunable.
    
    From: Chris Lahaye <dev@chrislahaye.com>
    Date: Tue, 26 Sep 2017 19:19:19 +0200
    Subject: pocket: Add hardware acceleration and properly maintain
     SYSTEM_UI flags
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -59,6 +59,7 @@ import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.os.WorkSource;
+import android.pocket.PocketManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.service.dreams.DreamManagerInternal;
@@ -2055,6 +2056,8 @@ public final class PowerManagerService extends SystemService
                 final int screenOffTimeout = getScreenOffTimeoutLocked(sleepTimeout);
                 final int screenDimDuration = getScreenDimDurationLocked(screenOffTimeout);
                 final boolean userInactiveOverride = mUserInactiveOverrideFromWindowManager;
+                final PocketManager pocketManager = (PocketManager) mContext.getSystemService(Context.POCKET_SERVICE);
+                final boolean isDeviceInPocket = pocketManager != null && pocketManager.isDeviceInPocket();
 
                 mUserActivitySummary = 0;
                 if (mLastUserActivityTime >= mLastWakeTime) {
@@ -2064,7 +2067,7 @@ public final class PowerManagerService extends SystemService
                         mUserActivitySummary = USER_ACTIVITY_SCREEN_BRIGHT;
                         if (mWakefulness == WAKEFULNESS_AWAKE) {
                             int buttonBrightness;
-                            if (mHardwareKeysDisable) {
+                            if (mHardwareKeysDisable || isDeviceInPocket) {
                                 buttonBrightness = 0;
                             } else if (mButtonBrightnessOverrideFromWindowManager >= 0) {
                                 buttonBrightness = mButtonBrightnessOverrideFromWindowManager;
